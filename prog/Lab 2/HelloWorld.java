7bdccf92b9116df94705ff6254d1c5f2a26d0b46
Jessica <j.m.morton@11h.net>
2014-01-14 15:32:00 -0800

    Yup

---
+++ prog/Lab 2/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+	
+	// main: generate some simple output
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world.");
+		System.out.println("How are you?");
+	}
+}
