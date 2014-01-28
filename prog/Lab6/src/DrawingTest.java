48dedaf6b39d21f074121054c3b32b28d0b7d596
Jessica <j.m.morton@11h.net>
2014-01-28 15:22:59 -0800

    lab6

--- prog/Lab6/src/DrawingTest.java
+++ prog/Lab6/src/DrawingTest.java
@@ -8,11 +8,40 @@ public class DrawingTest {
 		Graphics g = panel.getGraphics();
 		g.setColor(Color.DARK_GRAY);
 		g.fillRect(0, 0, 800, 600);
-		g.setColor(Color.WHITE);
-		g.drawLine(10, 10, 100, 100);
-		g.setColor(Color.MAGENTA);
-		// TODO: Add your code here. Hint: use g.fillRect
 		
+		// draw checkerboard grid
+		drawGrid(g, 200, 200, 3, 50);
+	
 		panel.compareToFilePublic();
+	}
+	
+	
+	public static void drawSquare(Graphics g, int x, int y, int size, java.awt.Color color) {
+		g.setColor(color);
+		g.fillRect(x, y, size, size);
+	}
+	
+	public static void drawRow(Graphics g, int x, int y, int squares, int size) {
+
+		for (int i = 1; i < squares; i++) {
+			drawSquare(g, x, y, size, Color.MAGENTA);
+			x += 100;
+		}
+	}
+	
+	public static void drawGrid(Graphics g, int x, int y, int squares, int size) {
+		
+		for (int i = 1; i <= squares; i++) {
+			
+			// if current row is even
+			if ((i % 2) == 0) {
+				drawSquare(g, x+size, y, size, Color.MAGENTA);
+			}
+			// if current row is odd
+			else {
+				drawRow(g, x, y, squares, size);
+			}
+			y += size;
+		} 	
 	}
 }
